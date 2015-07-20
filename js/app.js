c5e72e3fce913217c518f6cd0fd548a82426aecc
Victor Costa <victorcostaguitar@gmail.com>
2015-07-20 14:45:00 -0300

    add icon and splash

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform ,$rootScope ,$cordovaNetwork ) {
     $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
@@ -19,8 +19,21 @@ angular.module('starter', ['ionic', 'starter.controllers'])
           // org.apache.cordova.statusbar required
           StatusBar.styleDefault();
         }
+
+        if (window.Connection) {
+            $rootScope.$on('$cordovaNetwork:online', function(event, networkState) {
+                alert('online good sir');
+            })
+            $rootScope.$on('$cordovaNetwork:offline', function(event, networkState) {
+                alert('offline good sir');
+            })
+        }
+
     });
 })
+
+
+
 
 .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
