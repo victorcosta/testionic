0f586f82c143244817cfc003f1f67f0e1aacb7aa
Victor Costa <victorcostaguitar@gmail.com>
2015-08-05 09:22:17 -0300

    removing map

--- js/controllers.js
+++ js/controllers.js
@@ -67,6 +67,7 @@ angular.module('starter.controllers',  [])
     .success(function(data, status) {
         $scope.loading = false;
         $scope.subcategories = data.object;
+        
     })
     .error(function(data, status) {
         console.log(data || "Request failed");
@@ -173,10 +174,10 @@ angular.module('starter.controllers',  [])
 
 .controller('LocationsCtrl', ['$scope','$http', function($scope, $http) {
     $scope.loading = true;
-    $http({method: 'JSONP', url: "http://www.liquidanatal.com/app/stores.json?callback=JSON_CALLBACK", responseType: "json"})
+    $http({method: 'JSONP', url: "http://www.liquidanatal.com/app/hubs.json?callback=JSON_CALLBACK", responseType: "json"})
     .success(function(data, status) {
         $scope.loading = false;
-        $scope.stores = data.object;
+        $scope.hubs = data.object;
     })
     .error(function(data, status) {
         console.log(data || "Request failed");
@@ -189,96 +190,97 @@ angular.module('starter.controllers',  [])
 
 .controller('ProductlocationCtrl', ['$scope', '$sce', '$http' ,'$stateParams', '$filter', function($scope, $sce, $http, $stateParams, $filter) {
     $scope.loading = true;
-    $http({method: 'JSONP', url: "http://www.liquidanatal.com/app/categories/"+$stateParams.subcategoryId+".json?callback=JSON_CALLBACK", responseType: "json"})
+    $http({method: 'JSONP', url: "http://www.liquidanatal.com/app/hubs/"+$stateParams.locationId+".json?callback=JSON_CALLBACK", responseType: "json"})
     .success(function(data, status) {
         $scope.loading = false;
-        $scope.categories = data.object;
+        $scope.hubs = data.object;
     })
     .error(function(data, status) {
         console.log(data || "Request failed");
     });
-    $scope.searchFilter = function (produto) {
+    $scope.searchFilter = function (stores) {
         var keyword = new RegExp($scope.nameFilter, 'i');
-        return !$scope.nameFilter || keyword.test(produto.name);
+        return !$scope.nameFilter || keyword.test(hubs.Store.name);
     };
 }])
 
-.controller('MapCtrl', ['$scope', '$filter', '$http','$location', '$state', function ($scope, $filter, $http, $location, $state) {
-    $scope.loading = true;
+// .controller('MapCtrl', ['$scope', '$filter', '$http','$location', '$state', function ($scope, $filter, $http, $location, $state) {
+//     $scope.loading = true;
     
 
-    var createMarker = function(i, objStores, idKey) {
+//     var createMarker = function(i, objStores, idKey) {
 
-        if (idKey == null) {
-            idKey = "id";
-        }
+//         if (idKey == null) {
+//             idKey = "id";
+//         }
 
-        var latitude = objStores[i].Store.latitude;
-        var longitude = objStores[i].Store.longitude;
-        var ret = {
-            latitude:   latitude,
-            longitude:  longitude,
-            title:      objStores[i].Store.name,
-            address:    objStores[i].Store.address,
-            storeId:    objStores[i].Store.id,
-            icon:       'http://www.liquidanatal.com/site/img/pin.png',
-            // options:    {title:objStores[i].Store.name,content:objStores[i].Store.name,}
-        };
+//         var latitude = objStores[i].Store.latitude;
+//         var longitude = objStores[i].Store.longitude;
+//         var ret = {
+//             latitude:   latitude,
+//             longitude:  longitude,
+//             title:      objStores[i].Store.name,
+//             address:    objStores[i].Store.address,
+//             storeId:    objStores[i].Store.id,
+//             icon:       'http://www.liquidanatal.com/site/img/pin.png',
+//             // options:    {title:objStores[i].Store.name,content:objStores[i].Store.name,}
+//         };
 
-        ret.onClick = function(e) {
-            // $scope.modaltitle   = e.model.title;
-            // $scope.modaladdress = e.model.address;            
-            // $scope.modalstoreid = e.model.storeId;            
-            // $('#modalpin').openModal();
-            // console.log(e);
-            // $state.go('/stores/'+e.model.storeId);
-            $state.go('app.single', {storeId: e.model.storeId});
+//         ret.onClick = function(e) {
+//             // $scope.modaltitle   = e.model.title;
+//             // $scope.modaladdress = e.model.address;            
+//             // $scope.modalstoreid = e.model.storeId;            
+//             // $('#modalpin').openModal();
+//             // console.log(e);
+//             // $state.go('/stores/'+e.model.storeId);
+//             $state.go('app.single', {storeId: e.model.storeId});
+//             alert(e.model.storeId);
 
-        };
-        ret[idKey] = i;
-        return ret;
-    };
+//         };
+//         ret[idKey] = i;
+//         return ret;
+//     };
     
 
-    //Mapa    
-    $http({method: 'JSONP', url: "http://www.liquidanatal.com/app/stores.json?callback=JSON_CALLBACK", responseType: "json"})
-        .success(function (result) {
-            $scope.loading = false;
-            $scope.stores = result.object;
+//     //Mapa    
+//     $http({method: 'JSONP', url: "http://www.liquidanatal.com/app/stores.json?callback=JSON_CALLBACK", responseType: "json"})
+//         .success(function (result) {
+//             $scope.loading = false;
+//             $scope.stores = result.object;
 
-            $scope.markers = [];
+//             $scope.markers = [];
 
-            $scope.map = {
-                center: {
-                    latitude: -5.7999189,
-                    longitude: -35.2222448
-                },
-                zoom: 12,
-                bounds: {}
-            };
+//             $scope.map = {
+//                 center: {
+//                     latitude: -5.7999189,
+//                     longitude: -35.2222448
+//                 },
+//                 zoom: 12,
+//                 bounds: {}
+//             };
                   
 
 
-            $scope.$watch(function() {
-                return $scope.map.bounds;
-            }, function(nv, ov) {
-                if (!ov.southwest && nv.southwest) {
-                    var markers = [];
-                    for (var i = 0; i < result.object.length; i++) {
-                        markers.push(createMarker(i, result.object))
-                    }
-                    $scope.markers = markers;
-                }
-            }, true);
+//             $scope.$watch(function() {
+//                 return $scope.map.bounds;
+//             }, function(nv, ov) {
+//                 if (!ov.southwest && nv.southwest) {
+//                     var markers = [];
+//                     for (var i = 0; i < result.object.length; i++) {
+//                         markers.push(createMarker(i, result.object))
+//                     }
+//                     $scope.markers = markers;
+//                 }
+//             }, true);
 
 
-        })
-        .error(function (data, status) {
-            console.log(data);
-        });
+//         })
+//         .error(function (data, status) {
+//             console.log(data);
+//         });
 
 
-}])
+// }])
 
 .filter('trustAsResourceUrl', ['$sce', function($sce) {
     return function(val) {
